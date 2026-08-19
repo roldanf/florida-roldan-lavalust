6dcdd74b19999d665eda8de0d9b48ece1d26a8a1
roldanf <rldnflorida@gmail.com>
2026-08-19 14:17:37 +0800

    Update home.php

--- app/views/student/home.php
+++ app/views/student/home.php
@@ -6,7 +6,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Student Information</title>
+	<title>Florida's Home</title>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link href="https://fonts.googleapis.com/css2?family=Sora:wght@400;500;600;700;800&family=JetBrains+Mono:wght@500;600&display=swap" rel="stylesheet">
 	<style>
