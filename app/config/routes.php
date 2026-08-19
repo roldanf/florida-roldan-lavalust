af5304f8396296a75e4657a85d7203eb7b4522b7
roldanf <rldnflorida@gmail.com>
2026-08-19 11:57:04 +0800

    Update routes.php

--- app/config/routes.php
+++ app/config/routes.php
@@ -45,3 +45,13 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 /** @var object $router **/
 
 $router->get('/', 'Welcome::index');
+
+/*
+| -------------------------------------------------------------------
+| Student Information Page Routes
+| -------------------------------------------------------------------
+*/
+$router->get('/student', 'StudentController::index');
+
+$router->get('/student/profile', 'StudentController::profile')
+	   ->middleware('student.access');
