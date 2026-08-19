f5341c0e8fd5517b910e9ab94b565e7abbe12a54
roldanf <rldnflorida@gmail.com>
2026-08-19 13:58:06 +0800

    Update StudentController.php

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -45,7 +45,7 @@ class StudentController extends Controller
 				'instagram' => 'https://www.instagram.com/its.danskieee/',
 			    'tiktok'    => 'https://www.tiktok.com/@roldan.florida?lang=en',
 				'github'    => 'https://github.com/roldanf',
-				'teams'     => 'florida.roldano@minsu.edu.ph',
+				'linkedin'     => 'https://www.linkedin.com/in/roldan-florida-13124142b/',
 			],
 		];
 	}
