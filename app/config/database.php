7a1c0d19dd5dfb9824afbee354566a0637c6ba92
roldanf <rldnflorida@gmail.com>
2026-09-02 23:20:26 +0800

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'    => 'mysql',
-    'hostname'  => getenv('DB_HOST') ?: 'localhost',
-    'port'      => getenv('DB_PORT') ?: '3306',
-    'username'  => getenv('DB_USERNAME') ?: 'root',
-    'password'  => getenv('DB_PASSWORD') ?: '',
-    'database'  => getenv('DB_NAME') ?: 'mydb',
+    'hostname'  => getenv('DB_HOST') ?: ,
+    'port'      => getenv('DB_PORT') ?:,
+    'username'  => getenv('DB_USERNAME') ?: ,
+    'password'  => getenv('DB_PASSWORD') ?: ,
+    'database'  => getenv('DB_NAME') ?:,
     'charset'   => 'utf8mb4',
     'dbprefix'  => '',
     // Optional for SQLite
