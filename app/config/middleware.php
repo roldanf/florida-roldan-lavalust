4d7049aa0d15e5bc416b8d20c6d8cfb0fd57f7ff
roldanf <rldnflorida@gmail.com>
2026-09-09 21:16:36 +0800

    Update middleware.php

--- app/config/middleware.php
+++ app/config/middleware.php
@@ -42,7 +42,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 |
 */
 require_once APP_DIR . 'middlewares/StudentMiddleware.php';
-<<<<<<< HEAD
+
 require_once APP_DIR . 'middlewares/AuthMiddleware.php';
 require_once APP_DIR . 'middlewares/AdminMiddleware.php';
 
@@ -54,5 +54,5 @@ $config['middlewares'] = [
 
 $config['middlewares'] = [
 	'student.access' => new StudentMiddleware(),
->>>>>>> 701aab6aad5a1b25657c38ecb3e3f03579662e8b
+701aab6aad5a1b25657c38ecb3e3f03579662e8b
 ];
